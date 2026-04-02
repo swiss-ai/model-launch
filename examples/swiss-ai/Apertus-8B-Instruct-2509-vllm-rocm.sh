db4d4c0e230bbfac32ed4988fdd338bcc9a20d6f
robmsmt <rob@robmsmt.com>
2026-04-02 18:04:40 +0200

    name

--- examples/swiss-ai/Apertus-8B-Instruct-2509-vllm-rocm.sh
+++ examples/swiss-ai/Apertus-8B-Instruct-2509-vllm-rocm.sh
@@ -8,6 +8,6 @@ sml advanced \
   --slurm-time "05:00:00" \
   --partition mi300 \
   --framework-args "--model /capstor/store/cscs/swissai/infra01/hf_models/models/swiss-ai/Apertus-8B-Instruct-2509 \
-    --served-model-name swiss-ai/Apertus-8B-Instruct-2509-rocm2 \
+    --served-model-name swiss-ai/Apertus-8B-Instruct-2509-rocm \
     --host 0.0.0.0 \
     --port 8080"
