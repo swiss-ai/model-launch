931d79908e907f5e87ef2ae9ca7811384fac8489
robmsmt <rob@robmsmt.com>
2026-04-09 11:16:43 +0200

    add amd rocm

---
+++ examples/cli/swiss-ai/Apertus-70B-Instruct-2509-sglang-rocm-disable-mmap-mem-fraction-05.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sml advanced \
+  --slurm-nodes 1 \
+  --serving-framework sglang \
+  --slurm-environment src/swiss_ai_model_launch/assets/envs/sglang_rocm.toml \
+  --slurm-time "12:00:00" \
+  --partition mi300 \
+  --framework-args "--model /capstor/store/cscs/swissai/infra01/hf_models/models/swiss-ai/Apertus-70B-Instruct-2509 \
+    --served-model-name swiss-ai/Apertus-70B-Instruct-2509-rocm-sglang-disable-mmap-mem-fraction-05 \
+    --host 0.0.0.0 \
+    --port 8080 --tp-size 4 \
+    --weight-loader-disable-mmap \
+    --mem-fraction-static 0.5 "
---
+++ examples/cli/swiss-ai/Apertus-70B-Instruct-2509-vllm-rocm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sml advanced \
+  --slurm-nodes 1 \
+  --serving-framework vllm \
+  --slurm-environment src/swiss_ai_model_launch/assets/envs/vllm_rocm.toml \
+  --slurm-time "12:00:00" \
+  --partition mi300 \
+  --framework-args "--model /capstor/store/cscs/swissai/infra01/hf_models/models/swiss-ai/Apertus-70B-Instruct-2509 \
+    --served-model-name swiss-ai/Apertus-70B-Instruct-2509-rocm \
+    --host 0.0.0.0 \
+    --port 8080 --tensor-parallel-size 4 --gpu-memory-utilization 0.85"
