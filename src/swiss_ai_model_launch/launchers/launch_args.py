b2867a436c303a66b6e425426ccc06d64e103d7a
Burak Emir Sezen <burakemirsezen@gmail.com>
2026-04-28 14:31:52 +0200

    Feature/DCGM Exporter (#98)

--- src/swiss_ai_model_launch/launchers/launch_args.py
+++ src/swiss_ai_model_launch/launchers/launch_args.py
@@ -25,6 +25,9 @@ class LaunchArgs(BaseModel):
     telemetry_endpoint: str | None = None
     metrics_remote_write_url: str = "https://prometheus-dev.swissai.svc.cscs.ch/api/v1/write"
     metrics_agent_binary: str = "/capstor/store/cscs/swissai/infra01/ocf-share/vmagent"
+    dcgm_exporter_binary: str = "/capstor/store/cscs/swissai/infra01/ocf-share/dcgm-exporter"
+    disable_dcgm_exporter: bool = False
+    disable_metrics: bool = False
 
     @model_validator(mode="after")
     def set_defaults(self) -> "LaunchArgs":
