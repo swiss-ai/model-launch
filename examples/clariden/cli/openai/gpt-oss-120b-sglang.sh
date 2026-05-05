2cfd0f68699ddacd7c1546fa41f4e5bc145a4067
Qiaoyuan ZHENG <qiaoyuan.zheng@foxmail.com>
2026-05-05 13:40:56 +0200

    Add suggested launch config for gpt oss 120b

--- examples/clariden/cli/openai/gpt-oss-120b-sglang.sh
+++ examples/clariden/cli/openai/gpt-oss-120b-sglang.sh
@@ -6,9 +6,9 @@ sml advanced \
   --serving-framework sglang \
   --worker-port 8080 \
   --slurm-environment src/swiss_ai_model_launch/assets/envs/sglang.toml \
-  --framework-args "--model-path /capstor/store/cscs/swissai/infra01/hf_models/models/openai/gpt-oss-20b \
+  --framework-args "--model-path /capstor/store/cscs/swissai/infra01/hf_models/models/openai/gpt-oss-120b \
     --host 0.0.0.0 \
     --port 8080 \
-    --served-model-name openai/gpt-oss-20b-$(whoami) \
+    --served-model-name openai/gpt-oss-120b-$(whoami) \
     --dp-size 4 \
     --enable-metrics"
