59864c24f7999b07a5e17b00aca74e99999edffa
robmsmt <rob@robmsmt.com>
2026-05-18 16:02:18 +0200

    add more time

--- examples/clariden/cli/swiss-ai/Apertus-8B-Instruct-2509-sglang-dev.sh
+++ examples/clariden/cli/swiss-ai/Apertus-8B-Instruct-2509-sglang-dev.sh
@@ -6,6 +6,7 @@ sml advanced \
   --partition normal \
   --serving-framework sglang \
   --slurm-reservation SD-69241-apertus-1-5-0 \
+  --slurm-time 06:00:00 \
   --slurm-environment src/swiss_ai_model_launch/assets/envs/sglang.toml \
   --framework-args "--model-path /capstor/store/cscs/swissai/infra01/hf_models/models/swiss-ai/Apertus-8B-Instruct-2509 \
     --served-model-name swiss-ai/Apertus-8B-Instruct-2509-$(whoami)-dev \
