a7a3d6dd7948b27f6ef22ed0326c526418312b43
robmsmt <rob@robmsmt.com>
2026-06-03 11:11:24 +0200

    added apertus1.5 8b sft dpo example

---
+++ examples/clariden/cli/swiss-ai/1p5-experiments/Apertus-1.5-8B-sft-dpo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# from https://swissai-initiative.slack.com/archives/C0A9JJ7C5K6/p1780418221820059
+
+sml advanced \
+  --firecrest-system clariden \
+  --partition normal \
+  --slurm-nodes 1 \
+  --slurm-time 12:00:00 \
+  --serving-framework vllm \
+  --slurm-environment src/swiss_ai_model_launch/assets/envs/vllm_apertus_1.5.toml \
+  --framework-args "--model /capstor/store/cscs/swissai/infra01/models/Alignment/ap_1p5/ap1p5-8b-sft-256k-adam-lr6e-5-constant-128n_4200-online-lr5e-6-beta0.1-bs256-lenNormfalse-maxPL2048-rollout8-images-2453762-2453767 \
+    --served-model-name swiss-ai/Apertus-1.5-8B-Instruct-sft-dpo \
+    --tokenizer /capstor/store/cscs/swissai/infra01/hf_tokenizers/tokenizers/Apertus-v1p5-tool_output_toks-think_toks \
+    --tensor-parallel-size 4 \
+    --host 0.0.0.0 \
+    --port 8080 \
+    --trust-remote-code \
+    --trust-request-chat-template \
+    --max-model-len 256000 \
+    --gpu-memory-utilization 0.6 \
+    --skip-mm-profiling"
