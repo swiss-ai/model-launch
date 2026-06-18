d3b27d7dc636a677b6891ed08d58dc87d57cdbbd
robmsmt <rob@robmsmt.com>
2026-06-18 12:25:47 +0200

    added think as seperate model

--- examples/clariden/cli/swiss-ai/1p5-experiments/Apertus-1.5-8B-rl-stage2_notools_mixthink_VS_nothink_1606_480it.sh
+++ examples/clariden/cli/swiss-ai/1p5-experiments/Apertus-1.5-8B-rl-stage2_notools_mixthink_VS_nothink_1606_480it.sh
@@ -20,9 +20,27 @@ sml advanced \
     --trust-request-chat-template \
     --max-model-len 262144 \
     --gpu-memory-utilization 0.6 \
+    --skip-mm-profiling"
+    
+# THINK - swiss-ai/Apertus-1.5-8B-rl-stage2_notools_mixthink_1606_480it-THINK
+sml advanced \
+  --firecrest-system clariden \
+  --partition normal \
+  --slurm-nodes 1 \
+  --slurm-time 12:00:00 \
+  --serving-framework vllm \
+  --slurm-environment src/swiss_ai_model_launch/assets/envs/vllm_apertus_1.5.toml \
+  --framework-args "--model /capstor/store/cscs/swissai/infra01/models/rleval/rl_1p5-8b-stage2_notools_mixthink_1606_480it \
+    --served-model-name swiss-ai/Apertus-1.5-8B-rl-stage2_notools_mixthink_1606_480it-THINK \
+    --tokenizer /capstor/store/cscs/swissai/infra01/hf_tokenizers/tokenizers/Apertus-v1p5-tool_output_toks-think_toks \
+    --tensor-parallel-size 4 \
+    --host 0.0.0.0 \
+    --trust-remote-code \
+    --trust-request-chat-template \
+    --max-model-len 262144 \
+    --gpu-memory-utilization 0.6 \
     --skip-mm-profiling \
     --reasoning-parser deepseek_r1"
-    
 
 # NOT USED - we will instead make a virtual model in OWUI set thinking on the virtual one:
 #--default-chat-template-kwargs.enable_thinking true"
@@ -44,5 +62,4 @@ sml advanced \
     --trust-request-chat-template \
     --max-model-len 262144 \
     --gpu-memory-utilization 0.6 \
-    --skip-mm-profiling \
-    --reasoning-parser deepseek_r1"
+    --skip-mm-profiling"
