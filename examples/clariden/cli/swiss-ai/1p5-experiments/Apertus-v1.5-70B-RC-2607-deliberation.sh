f276f06ab777ebb25fef9ec2c49e66e4f767af6f
Aryan Ahadinia <aryan.ahadinia@epfl.ch>
2026-07-14 10:47:50 +0200

    Update RC Launch Scripts

--- examples/clariden/cli/swiss-ai/1p5-experiments/Apertus-v1.5-70B-RC-2607-deliberation.sh
+++ examples/clariden/cli/swiss-ai/1p5-experiments/Apertus-v1.5-70B-RC-2607-deliberation.sh
@@ -3,11 +3,13 @@ sml advanced \
   --tui \
   --partition normal \
   --framework vllm \
+  --time 12:00:00 \
   --environment src/swiss_ai_model_launch/assets/envs/vllm_apertus_1.5.toml \
   --framework-args "--model /capstor/store/cscs/swissai/infra01/hf_models/ahadinia_models/apertus-ai/Apertus-v1.5-70B-RC-2607 \
-    --served-model-name apertus-ai/Apertus-v1.5-70B-RC-2607-deliberation \
+    --served-model-name apertus-ai/Apertus-v1.5-70B-RC-deliberation \
     --tensor-parallel-size 4 \
     --trust-remote-code \
+    --gpu-memory-utilization 0.8 \
     --skip-mm-profiling \
     --max-model-len 262144 \
     --enable-auto-tool-choice \
