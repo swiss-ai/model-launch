308d2a1da36a5ebbbdb98444c0b7daff014ae04c
Aryan Ahadinia <aryan.ahadinia@epfl.ch>
2026-08-25 14:03:18 +0200

    Restore Deleted Models

---
+++ examples/clariden/cli/arcee-ai/Trinity-Mini-vllm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sml advanced \
+  --system clariden \
+  --partition normal \
+  --framework vllm \
+  --environment src/swiss_ai_model_launch/assets/envs/vllm.toml \
+  --framework-args "--model /capstor/store/cscs/swissai/infra01/hf_models/models/arcee-ai/Trinity-Mini \
+    --served-model-name arcee-ai/Trinity-Mini \
+    --host 0.0.0.0 \
+    --enable-auto-tool-choice \
+    --reasoning-parser deepseek_r1 \
+    --tool-call-parser hermes"
---
+++ examples/clariden/cli/mistralai/Mixtral-8x22B-Instruct-v0.1-sglang.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sml advanced \
+  --system clariden \
+  --partition normal \
+  --nodes-per-replica 2 \
+  --framework sglang \
+  --environment src/swiss_ai_model_launch/assets/envs/sglang.toml \
+  --framework-args "--model /capstor/store/cscs/swissai/infra01/hf_models/models/mistralai/Mixtral-8x22B-Instruct-v0.1 \
+    --host 0.0.0.0 \
+    --tp-size 8 \
+    --served-model-name mistralai/Mixtral-8x22B-Instruct-v0.1 \
+    --enable-metrics"
